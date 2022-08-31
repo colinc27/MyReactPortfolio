aa7d6a33e2625777a8bd048470732d503e49bd40
colinc27 <colinchristoph27@gmail.com>
2022-08-31 07:58:38 -0700

    Update Intro.js

--- myreactportfolio/src/components/Intro.js
+++ myreactportfolio/src/components/Intro.js
@@ -6,11 +6,13 @@ const Intro = () => {
       <h1>Introduction</h1>
       <strong>
         <p className="py-2">
-          I am a detail-oriented, organized and meticulous professional. Works
-          at fast pace to meet tight deadlines. Enthusiastic team player ready
-          to contribute to company success. I have a very unique experience that
-          makes me as well rounded as you can be and I think that will make me a
-          better software engineer/developer.
+          I am a detail-oriented, organized and meticulous professional. I work
+          at a fast pace to meet tight deadlines. I'm an enthusiastic team
+          player ready to contribute to company success. I have a very unique
+          experience that makes me as well rounded as you can be and I think
+          that will make me a great software engineer/developer. I am willing to
+          learn anything and will do so quickly. I can be a great asset for your
+          business that can wear multiple hats and produce excellent work.
         </p>
       </strong>
 
@@ -20,12 +22,12 @@ const Intro = () => {
           I was born and raised in Wauconda, IL. After high school, I went to
           Iowa State University where I studied mechanical engineering. I
           graduated Magna Cumme Laude with a GPA of 3.66. After being an
-          engineer for a couple years I decided to move away from Illinois to AZ
-          and began a job at rocket mortgage where I developed my communication
-          skills deeply. During my time at rocket mortgage, I studied and
-          graduated at Michigan State University with a coding bootcamp
-          certificate in the MERN tech stack. I am now pursuing a career in
-          software development.
+          engineer for a couple years, I decided to move away from Illinois to
+          AZ and began a job at rocket mortgage where I developed my
+          communication skills deeply. During my time at rocket mortgage, I
+          studied and graduated at Michigan State University with a coding
+          bootcamp certificate in the MERN tech stack. I am now pursuing a
+          career in software development.
         </p>
       </strong>
     </div>
