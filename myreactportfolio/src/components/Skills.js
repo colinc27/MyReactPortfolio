ee8eb7a16c8b3f2a99724c31cb710536dae1c80c
colinc27 <colinchristoph27@gmail.com>
2022-08-30 20:52:38 -0700

    update skills

--- myreactportfolio/src/components/Skills.js
+++ myreactportfolio/src/components/Skills.js
@@ -43,19 +43,73 @@ const Experience = () => {
             </ul>
           </Accordion.Body>
         </Accordion.Item>
-        <Accordion.Item eventKey="1" className="bg-primary">
+        <Accordion.Item eventKey="1" className="">
           <Accordion.Header>Engineering</Accordion.Header>
           <Accordion.Body>
-            <ul>
-              <li></li>
+            <ul className="nodots">
+              <strong>
+                {skill.map((skill) => {
+                  if (skill.type === "engineering") {
+                    return (
+                      <li className="py-2">
+                        <Container>
+                          <Row>
+                            <Col xs lg="2">
+                              <i className={skill.svg}></i>
+                              {skill.name}
+                            </Col>
+                            <Col xs lg="8" md="auto">
+                              <ProgressBar
+                                className=""
+                                label={`${skill.yearsExp} Year exp.`}
+                                now={skill.yearsExp}
+                                min={0}
+                                max={3}
+                              />
+                            </Col>
+                          </Row>
+                        </Container>
+                      </li>
+                    );
+                  }
+                  return null;
+                })}
+              </strong>
             </ul>
           </Accordion.Body>
         </Accordion.Item>
-        <Accordion.Item eventKey="2" className="bg-primary">
+        <Accordion.Item eventKey="2" className="">
           <Accordion.Header>General</Accordion.Header>
           <Accordion.Body>
-            <ul>
-              <li></li>
+            <ul className="nodots">
+              <strong>
+                {skill.map((skill) => {
+                  if (skill.type === "general") {
+                    return (
+                      <li className="py-2">
+                        <Container>
+                          <Row>
+                            <Col xs lg="2">
+                              <i className={skill.svg}></i>
+                              {skill.name}
+                            </Col>
+                            <Col xs lg="8" md="auto">
+                              <ProgressBar
+                                className=""
+                                label={`${skill.yearsExp} Year exp.`}
+                                now={skill.yearsExp}
+                                min={0}
+                                max={3}
+                              />
+                            </Col>
+                          </Row>
+                        </Container>
+                      </li>
+                    );
+                  }
+                  return null;
+                })}
+              </strong>
             </ul>
           </Accordion.Body>
         </Accordion.Item>
